0e6a5ac1639289da7c9dce07a20d2cc0d2620868
“sxx” <1640294571@qq.com>
2024-09-02 14:58:22 +0800

    init

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="header">
+    <ul>
+      <li>React</li>
+      <li>Vue</li>
+      <li>计算机基础</li>
+      <li>数据结构与算法</li>
+      <li>源码系列</li>
+      <li>项目</li>
+      <li>go</li>
+      <li>数据库</li>
+    </ul>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Header',
+  components: {
+  },
+});
+</script>
